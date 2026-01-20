4ea92e6bec4c679c802b3210c185b066b62726b2
peekitn <franciscoafbatalha@hotmail.com>
2026-01-20 11:24:47 -0300

    finish

--- index.js
+++ index.js
@@ -1,12 +1,62 @@
 import express from "express";
 
+
 const app = express();
 const port = 3000;
+
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+
+let posts = [];
+let idCounter = 1;
+
+
+// Home - View all posts
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+res.render("index.ejs", { posts });
 });
 
+
+// Form to create post
+app.get("/new", (req, res) => {
+res.render("new.ejs");
+});
+
+
+// Create post
+app.post("/new", (req, res) => {
+const { title, content } = req.body;
+posts.push({ id: idCounter++, title, content });
+res.redirect("/");
+});
+
+
+// Edit form
+app.get("/edit/:id", (req, res) => {
+const post = posts.find(p => p.id == req.params.id);
+res.render("edit.ejs", { post });
+});
+
+
+// Update post
+app.post("/edit/:id", (req, res) => {
+const post = posts.find(p => p.id == req.params.id);
+post.title = req.body.title;
+post.content = req.body.content;
+res.redirect("/");
+});
+
+
+// Delete post
+app.post("/delete/:id", (req, res) => {
+posts = posts.filter(p => p.id != req.params.id);
+res.redirect("/");
+});
+
+
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+console.log(`Server running on port ${port}`);
 });
